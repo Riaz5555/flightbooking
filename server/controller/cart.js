4f14db459cfd87f7a8959aeca7270e1060325857
Riaz5555 <mdsydulislamriaz@gmail.com>
2023-05-13 00:02:40 +0600

    11th commit

--- server/controller/cart.js
+++ server/controller/cart.js
@@ -119,3 +119,47 @@ module.exports.getItems = (req, res) => {
             });
         }
       }
+      
+      delay(500).then(() => {
+        res.json(output);
+      });
+    })
+    .catch((err) => {
+      // console.log("Error in fetching cart data.");
+      res.status(404).json({ error: "Unable to find info" });
+    });
+};
+
+module.exports.addItem = (req, res) => {
+  const item = new Cart({
+    type: req.body.type,
+    userId: req.body.userId,
+    itemId: req.body.itemId,
+    price: req.body.price,
+  });
+
+  const result = item
+    .save()
+    .then(() => {
+      // console.log("Item is added to the cart.");
+    })
+    .catch(() => {
+      // console.log("Item insertion failed!");
+    });
+
+  res.json(result);
+};
+
+module.exports.deleteItem = (req, res) => {
+  const cartId = req.params.cartId;
+
+  const result = Cart.deleteOne({ _id: new mongodb.ObjectId(cartId) })
+    .then(() => {
+      // console.log("Item is deleted.");
+    })
+    .catch(() => {
+      // console.log("Failed to delete item!");
+    });
+
+  res.json(result);
+};
